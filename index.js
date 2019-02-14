8a548f694af7cd4fec4c5fc8fa9ad3aa587bf545
Matt Payne <Payne@MattPayne.org>
2019-02-13 21:39:36 -0600

    node index.js # writes output.txt now.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-
 const puppeteer = require('puppeteer');
+const fs = require('fs-extra');
 
 (async function main() {
   try {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     const maxPages = 13;
+    const outputFileName = 'output.txt';
+    await fs.writeFile(outputFileName, 'TechOmaha Video info....\n');
     for (let pageNumber = 1; pageNumber <= maxPages; pageNumber++) {
       await page.goto(`http://techomaha.com/videos/page/${pageNumber}`);
       await page.waitForSelector('article');
@@ -15,6 +17,7 @@ const puppeteer = require('puppeteer');
       for (const article of articles) {
         const href = await article.$eval('a', a => a.href);
         console.log(href);
+        await fs.appendFile(outputFileName, `${href}\n`);
         if (href.includes('page')) {
           nextHref = href;
         }
