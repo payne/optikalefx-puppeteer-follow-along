f11d9df967f6d8e2df06415a03b9e79e922e2d0b
Matt Payne <Payne@MattPayne.org>
2019-02-13 22:28:48 -0600

    node index.js # makes output.json

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const fs = require('fs-extra');
     const maxPages = 13;
     const outputFileName = 'output.txt';
     await fs.writeFile(outputFileName, 'TechOmaha Video info....\n');
+    const videos = [];
     for (let pageNumber = 1; pageNumber <= maxPages; pageNumber++) {
       await page.goto(`http://techomaha.com/videos/page/${pageNumber}`);
       await page.waitForSelector('article');
@@ -16,15 +17,25 @@ const fs = require('fs-extra');
       let nextHref = '';
       for (const article of articles) {
         const href = await article.$eval('a', a => a.href);
+        let imgsrc = '';
+        try {
+          imgsrc = await article.$eval('img', img => img.src);
+        } catch (e) {
+          console.log(`did not find an img: `, e);
+        }
         const title = await article.$eval('.entry-title a', a => a.textContent);
+        const content = await article.$eval('.post-content p', p => p.textContent);
         console.log(href);
         console.log(`\t${title}`);
+        console.log(`\t${content}`);
+        videos.push({ href, imgsrc, title, content });
         await fs.appendFile(outputFileName, `${href}\n`);
         if (href.includes('page')) {
           nextHref = href;
         }
       }
     }
+    await fs.writeFile('output.json', JSON.stringify(videos));
     browser.close();
   } catch (e) {
     console.log('our error', e);
