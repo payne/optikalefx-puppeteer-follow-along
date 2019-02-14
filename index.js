ad515e82cc94b14a7eae86ed358e1a52f77853bb
Matt Payne <Payne@MattPayne.org>
2019-02-13 20:55:12 -0600

    Loops through all the video pages now.

--- index.js
+++ index.js
@@ -5,13 +5,20 @@ const puppeteer = require('puppeteer');
   try {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    await page.goto('http://techomaha.com/videos/');
-    await page.waitForSelector('article');
-    const articles = await page.$$('article');
-    console.log(`there are ${articles.length} articles.`);
-    for (const article of articles) {
-      const href = await article.$eval('a', a => a.href);
-      console.log(href);
+    const maxPages = 13;
+    for (let pageNumber = 1; pageNumber <= maxPages; pageNumber++) {
+      await page.goto(`http://techomaha.com/videos/page/${pageNumber}`);
+      await page.waitForSelector('article');
+      const articles = await page.$$('article');
+      console.log(`there are ${articles.length} articles.`);
+      let nextHref = '';
+      for (const article of articles) {
+        const href = await article.$eval('a', a => a.href);
+        console.log(href);
+        if (href.includes('page')) {
+          nextHref = href;
+        }
+      }
     }
   } catch (e) {
     console.log('our error', e);
